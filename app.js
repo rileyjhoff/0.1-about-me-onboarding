005aedf9ab8766edfd1705d20ce1c9c729d70e16
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-01 14:56:33 -0800

    clicking the button a second time will refresh page, hiding the animal data

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const button = document.getElementById('animal-button');
 const animalData = document.getElementById('animal-info');
 
-console.log(button, animalData);
+// console.log(button, animalData);
 
 // set event listeners 
     // get user input
@@ -13,5 +13,11 @@ console.log(button, animalData);
     // update DOM to reflect the new state
 
 button.addEventListener('click', () => {
-    animalData.classList.remove('hidden');
+    animalData.classList.replace('hidden', 'visible');
+    button.textContent = 'Hide Favorite Animal';
+    if (animalData.classList.contains('visible')) {
+        button.addEventListener('click', () => {
+            document.location.reload(true);
+        });
+    }
 });
