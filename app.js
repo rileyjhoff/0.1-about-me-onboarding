854af106bc157d8dd483a2495e20916d781db85d
Riley Hoffman <rileyjhoff@gmail.com>
2022-03-01 15:33:53 -0800

    added a couple more hidden buttons. Will show up when the button before it is clicked and hide once it is clicked

--- app.js
+++ app.js
@@ -2,22 +2,48 @@
 
 // grab DOM elements
 
-const button = document.getElementById('animal-button');
+let animalButton = document.getElementById('animal-button');
 const animalData = document.getElementById('animal-info');
 
-// console.log(button, animalData);
+const carButton = document.getElementById('car-button');
+const carData = document.getElementById('car-info');
+
+const foodButton = document.getElementById('food-button');
+const foodData = document.getElementById('food-info');
+
+const hobbyButton = document.getElementById('hobby-button');
+const hobbyData = document.getElementById('hobby-info');
 
 // set event listeners 
     // get user input
     // use user input to update state 
     // update DOM to reflect the new state
 
-button.addEventListener('click', () => {
+animalButton.addEventListener('click', () => {
     animalData.classList.replace('hidden', 'visible');
-    button.textContent = 'Hide Favorite Animal';
+    carButton.classList.replace('hidden', 'visible');
+    animalButton.textContent = 'Reset';
     if (animalData.classList.contains('visible')) {
-        button.addEventListener('click', () => {
+        animalButton.addEventListener('click', () => {
             document.location.reload(true);
         });
     }
+});
+
+carButton.addEventListener('click', () => {
+    carData.classList.replace('hidden', 'visible');
+    carButton.classList.replace('visible', 'hidden');
+    foodButton.classList.replace('hidden', 'visible');
+});
+
+foodButton.addEventListener('click', () => {
+    foodData.classList.replace('hidden', 'visible');
+    foodButton.classList.replace('visible', 'hidden');
+    hobbyButton.classList.replace('hidden', 'visible');
+});
+
+hobbyButton.addEventListener('click', () => {
+    hobbyData.classList.replace('hidden', 'visible');
+    hobbyButton.classList.replace('visible', 'hidden');
+    // hobbyButton.classList.replace('hidden', 'visible');
 });
